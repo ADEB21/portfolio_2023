92c5a6cfc73559fe381da25af99ab8ac0f2c876b
ADEB <elliurbedruhtra@gmail.com>
2023-02-20 21:59:29 +0100

    🔧💄💬📱 add images and nav and pages

--- src/components/molecules/ProjectsCard/ProjectsCard.jsx
+++ src/components/molecules/ProjectsCard/ProjectsCard.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import "./projectsCard.scss";
+import Picture from "../../atoms/Picture/Picture";
 
-function ProjectsCard({ title, date }) {
+
+function ProjectsCard({ title, date, img }) {
   return (
     <li className="ad_projectCard-item">
       <div className="ad_projectCard_text">
         <p className="ad_projectCard_text-title card-title">{title}</p>
         <p className="ad_projectCard_text-date date">{date}</p>
       </div>
-      <div className="ad_projectCard_picture"></div>
+      <div className="ad_projectCard_picture">
+        <Picture img={img}/>
+      </div>
     </li>
   );
 }

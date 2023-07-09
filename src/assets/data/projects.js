27d160c24960cba6e375f5b6a77ebb142c920c1e
Arthur <elliurbedruhtra@gmail.com>
2023-07-09 11:18:56 +0200

    changes

---
+++ src/assets/data/projects.js
@@ -0,0 +1,12 @@
+export default {
+    projects: [
+      {
+        title: "SCC",
+        slug: "scc",
+        subTitle:
+          "Ce parcours est parfait pour développer vos compétences en leadership et mettre en œuvre des innovations managériales idéales pour inspirer vos équipes et créer une culture d'innovation.",
+        description:
+          "Anticipez les besoins changeants en développant une culture d'innovation et créer de nouveaux mode de fonctionnement et de collaboration dans vos équipe.",
+      },
+    ],
+  };

a884b48351add25855be281cdde48628e7b16167
Arthur <elliurbedruhtra@gmail.com>
2023-07-09 17:30:04 +0200

    change typo

--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -1,84 +1,211 @@
 export default {
     projects: [
       {
-        title: "SCC",
-        slug: "scc",
-        subTitle:
-          "Ce parcours est parfait pour développer vos compétences en leadership et mettre en œuvre des innovations managériales idéales pour inspirer vos équipes et créer une culture d'innovation.",
-        description:
-          "Anticipez les besoins changeants en développant une culture d'innovation et créer de nouveaux mode de fonctionnement et de collaboration dans vos équipe.",
-      },
-      {
-        title: "Les Belles Quilles",
-        slug: "les-belles-quilles",
-        subTitle:
-          "Ce parcours est parfait pour développer vos compétences en leadership et mettre en œuvre des innovations managériales idéales pour inspirer vos équipes et créer une culture d'innovation.",
-        description:
-          "Anticipez les besoins changeants en développant une culture d'innovation et créer de nouveaux mode de fonctionnement et de collaboration dans vos équipe.",
-      },
-      {
         title: "Experience",
+        cover : {
+          picture : {
+            images : {
+              desktop: {
+                jpeg: ["",""],
+                webp: ["",""],
+              },
+              mobile: {
+                jpeg: ["",""],
+                webp: ["",""],
+              }
+            },
+            lazy: {
+              desktop : "",
+              mobile: ""
+            },
+            alternativeText : "Image de couverture du projet"
+          }
+        },
         slug: "experience",
-        subTitle:
-          "Ce parcours est parfait pour développer vos compétences en leadership et mettre en œuvre des innovations managériales idéales pour inspirer vos équipes et créer une culture d'innovation.",
-        description:
-          "Anticipez les besoins changeants en développant une culture d'innovation et créer de nouveaux mode de fonctionnement et de collaboration dans vos équipe.",
-      },
-      {
-        title: "Cambium",
-        slug: "cambium",
-        subTitle:
-          "Ce parcours est parfait pour développer vos compétences en leadership et mettre en œuvre des innovations managériales idéales pour inspirer vos équipes et créer une culture d'innovation.",
-        description:
-          "Anticipez les besoins changeants en développant une culture d'innovation et créer de nouveaux mode de fonctionnement et de collaboration dans vos équipe.",
-      },
-      {
-        title: "Incepto Avocats",
-        slug: "incepto-avocats",
-        subTitle:
-          "Ce parcours est parfait pour développer vos compétences en leadership et mettre en œuvre des innovations managériales idéales pour inspirer vos équipes et créer une culture d'innovation.",
-        description:
-          "Anticipez les besoins changeants en développant une culture d'innovation et créer de nouveaux mode de fonctionnement et de collaboration dans vos équipe.",
-      },
-      {
-        title: "Heliopolis",
-        slug: "heliopolis",
-        subTitle:
-          "Ce parcours est parfait pour développer vos compétences en leadership et mettre en œuvre des innovations managériales idéales pour inspirer vos équipes et créer une culture d'innovation.",
-        description:
-          "Anticipez les besoins changeants en développant une culture d'innovation et créer de nouveaux mode de fonctionnement et de collaboration dans vos équipe.",
-      },
-      {
-        title: "TertiaVia",
-        slug: "tertiavia",
-        subTitle:
-          "Ce parcours est parfait pour développer vos compétences en leadership et mettre en œuvre des innovations managériales idéales pour inspirer vos équipes et créer une culture d'innovation.",
-        description:
-          "Anticipez les besoins changeants en développant une culture d'innovation et créer de nouveaux mode de fonctionnement et de collaboration dans vos équipe.",
-      },
-      {
-        title: "Les suites de l'Île Barbe",
-        slug: "les-suites-de-lile-barbe",
-        subTitle:
-          "Ce parcours est parfait pour développer vos compétences en leadership et mettre en œuvre des innovations managériales idéales pour inspirer vos équipes et créer une culture d'innovation.",
-        description:
-          "Anticipez les besoins changeants en développant une culture d'innovation et créer de nouveaux mode de fonctionnement et de collaboration dans vos équipe.",
-      },
-      {
-        title: "Gongju",
-        slug: "gongju",
-        subTitle:
-          "Ce parcours est parfait pour développer vos compétences en leadership et mettre en œuvre des innovations managériales idéales pour inspirer vos équipes et créer une culture d'innovation.",
-        description:
-          "Anticipez les besoins changeants en développant une culture d'innovation et créer de nouveaux mode de fonctionnement et de collaboration dans vos équipe.",
-      },
-      {
-        title: "Web Component",
-        slug: "web-component",
-        subTitle:
-          "Ce parcours est parfait pour développer vos compétences en leadership et mettre en œuvre des innovations managériales idéales pour inspirer vos équipes et créer une culture d'innovation.",
-        description:
-          "Anticipez les besoins changeants en développant une culture d'innovation et créer de nouveaux mode de fonctionnement et de collaboration dans vos équipe.",
+        date: "2022",
+        projectDescription: {
+          title : "Une histoire qui dure depuis 1973",
+          goal: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus metus, mi cursus amet. Sit felis tellus praesent amet, suspendisse nisi at pretium. Tempus lectus facilisis nec viverra est, volutpat pulvinar non velit. Sem sit magna in augue libero ultrices. Elementum eros amet, vivamus id eget diam risus in mattis.",
+        },
+        marqueeImages: [
+          {
+            images : {
+              desktop: {
+                jpeg: ["",""],
+                webp: ["",""],
+              },
+              mobile: {
+                jpeg: ["",""],
+                webp: ["",""],
+              }
+            },
+            lazy: {
+              desktop : "",
+              mobile: ""
+            },
+            alternativeText : "Image de couverture du projet"
+          },
+          {
+            images : {
+              desktop: {
+                jpeg: ["",""],
+                webp: ["",""],
+              },
+              mobile: {
+                jpeg: ["",""],
+                webp: ["",""],
+              }
+            },
+            lazy: {
+              desktop : "",
+              mobile: ""
+            },
+            alternativeText : "Image de couverture du projet"
+          },
+          {
+            images : {
+              desktop: {
+                jpeg: ["",""],
+                webp: ["",""],
+              },
+              mobile: {
+                jpeg: ["",""],
+                webp: ["",""],
+              }
+            },
+            lazy: {
+              desktop : "",
+              mobile: ""
+            },
+            alternativeText : "Image de couverture du projet"
+          },
+          {
+            images : {
+              desktop: {
+                jpeg: ["",""],
+                webp: ["",""],
+              },
+              mobile: {
+                jpeg: ["",""],
+                webp: ["",""],
+              }
+            },
+            lazy: {
+              desktop : "",
+              mobile: ""
+            },
+            alternativeText : "Image de couverture du projet"
+          },
+          {
+            images : {
+              desktop: {
+                jpeg: ["",""],
+                webp: ["",""],
+              },
+              mobile: {
+                jpeg: ["",""],
+                webp: ["",""],
+              }
+            },
+            lazy: {
+              desktop : "",
+              mobile: ""
+            },
+            alternativeText : "Image de couverture du projet"
+          },
+          {
+            images : {
+              desktop: {
+                jpeg: ["",""],
+                webp: ["",""],
+              },
+              mobile: {
+                jpeg: ["",""],
+                webp: ["",""],
+              }
+            },
+            lazy: {
+              desktop : "",
+              mobile: ""
+            },
+            alternativeText : "Image de couverture du projet"
+          },
+          {
+            images : {
+              desktop: {
+                jpeg: ["",""],
+                webp: ["",""],
+              },
+              mobile: {
+                jpeg: ["",""],
+                webp: ["",""],
+              }
+            },
+            lazy: {
+              desktop : "",
+              mobile: ""
+            },
+            alternativeText : "Image de couverture du projet"
+          },
+          {
+            images : {
+              desktop: {
+                jpeg: ["",""],
+                webp: ["",""],
+              },
+              mobile: {
+                jpeg: ["",""],
+                webp: ["",""],
+              }
+            },
+            lazy: {
+              desktop : "",
+              mobile: ""
+            },
+            alternativeText : "Image de couverture du projet"
+          },
+          {
+            images : {
+              desktop: {
+                jpeg: ["",""],
+                webp: ["",""],
+              },
+              mobile: {
+                jpeg: ["",""],
+                webp: ["",""],
+              }
+            },
+            lazy: {
+              desktop : "",
+              mobile: ""
+            },
+            alternativeText : "Image de couverture du projet"
+          }
+        ],
+        process : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Iaculis euismod ipsum orci urna luctus arcu. Sodales lectus imperdiet sagittis eros, ornare quis. Sagittis egestas convallis pellentesque eget lectus id amet. Sodales a sit aliquet dis. Tincidunt ut nunc leo cursus tempor. Pharetra bibendum magna viverra id. Lobortis non sed quis purus venenatis quis augue id.",
+        colorStory: {
+          paragraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Iaculis euismod ipsum orci urna luctus arcu. Sodales lectus imperdiet sagittis eros, ornare quis. Sagittis egestas convallis pellentesque eget lectus id amet.",
+          colors : ["#79C5D7", "#FADD4B", "#F98E45", "#FFF5E5", "#1D1D1D", "#E85243"]
+        },
+        lastImage : {
+          picture : {
+            images : {
+              desktop: {
+                jpeg: ["",""],
+                webp: ["",""],
+              },
+              mobile: {
+                jpeg: ["",""],
+                webp: ["",""],
+              }
+            },
+            lazy: {
+              desktop : "",
+              mobile: ""
+            },
+            alternativeText : "Image de couverture du projet"
+          }
+        }
       },
     ],
   };

6b4d71893c74d04ab0bbd2949d10d25f684c8771
ADEB <elliurbedruhtra@gmail.com>
2024-10-23 14:26:52 +0200

    🔧 fix

--- src/components/molecules/Header/index.jsx
+++ src/components/molecules/Header/index.jsx
@@ -1,24 +1,28 @@
-import React from 'react'
-import headerStyle from "./header.module.scss"
-import Icons from "../../atoms/Icons"
+import React from "react";
+import headerStyle from "./header.module.scss";
+import Icons from "../../atoms/Icons";
 
 const index = () => {
   return (
     <header className={headerStyle.ad_header}>
-        <a href="/"><p className='ad-small-heading'>Arthur Debruille</p></a>
-        <nav>
-            <ul>
-                <a href="/">
-                    <li>Accueil</li>
-                </a><a href="/about">
-                    <li>A propos</li>
-                </a><a href="/contact">
-                    <li>Contact</li>
-                </a>
-            </ul>
-        </nav>
+      <a href="/">
+        <p className="ad-small-heading">Arthur Debruille</p>
+      </a>
+      <nav>
+        <ul>
+          <a href="/">
+            <li>Accueil</li>
+          </a>
+          {/* <a href="/about">
+            <li>A propos</li>
+          </a> */}
+          <a href="/contact">
+            <li>Contact</li>
+          </a>
+        </ul>
+      </nav>
     </header>
-  )
-}
+  );
+};
 
-export default index
+export default index;
